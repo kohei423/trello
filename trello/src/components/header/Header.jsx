0a9e096df34da58cfdf717efbd72e38f459f0387
kohei423 <kyamauch5476@gmail.com>
2023-07-23 16:42:01 +0900

    trello app

---
+++ trello/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <header>
+            <h1>
+                simple trello
+            </h1>
+        </header>
+    </div>
+  )
+}
